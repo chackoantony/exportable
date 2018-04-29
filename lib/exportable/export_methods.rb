fdb5d5a555f848fc0dc150a763fb0024c822bdda
Vishal Taj <getvishalonline@gmail.com>
2018-04-29 18:00:12 +0800

    added export_attributes method to export attribute list

--- lib/exportable/export_methods.rb
+++ lib/exportable/export_methods.rb
@@ -9,7 +9,11 @@ module Exportable
       unless Exportable.formats.include?(format.to_sym)
         raise ArgumentError, "Unsupported output format '#{format}'"
       end
-      send("export_#{format}", options)
+      send("export_#{format}", options.merge({methods: []}))
+    end
+
+    def export_attributes(options = {})
+      get_export_options(self, @options.merge(options))[:fields].map(&:to_s) rescue self.attribute_names
     end
   end
 end
